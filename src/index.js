b8a9fb26556eae9b42c4e932c345d6cb10703e7c
kkota <kota111709@outlook.jp>
2020-06-02 01:48:30 +0900

    first_commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+let state_value = {
+  counter: 0,
+  message: "COUNTER"
+}
+
+function counter(state = state_value, action) {
+  switch(action.type) {
+    case 'INCREMENT' :
+      return {
+        counter:state.counter + 1,
+        message: "INCREMENT"
+      };
+
+    case 'DECREMENT' :
+      return {
+        counter: state.counter - 1,
+        message: "DECREMENT"
+      };
+
+    case 'RESET' :
+      return {
+        counter: 0,
+        message: "RESET"
+      };
+
+    default :
+      return state;
+
+  }
+}
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, counter)
+
+let store = createStore(persistedReducer)
+let pstore = persistStore(store)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={<p>loading...</p>} persistor={pstore}>
+      <App></App>
+    </PersistGate>
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
